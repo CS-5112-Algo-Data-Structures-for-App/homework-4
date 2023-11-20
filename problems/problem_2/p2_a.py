d0b66b582ca19037a302d328d16ed6dfd03cc36e
Matthew Shen <mds377@cornell.edu>
2023-11-19 22:07:30 -0500

    problem 2 done

--- problems/problem_2/p2_a.py
+++ problems/problem_2/p2_a.py
@@ -1,2 +1,38 @@
 def linear_search(packages, boxes):
-  pass
+  suppliers_waste_lst = []
+  num_suppliers_fail_int = 0
+
+  for supplier in boxes:
+    supplier_waste_int = 0
+    sorted_boxes = sorted(supplier)
+
+    for package in packages:
+      if package > sorted_boxes[-1]:
+        num_suppliers_fail_int += 1
+        break
+
+      for box in sorted_boxes:
+        if package <= box:
+          supplier_waste_int += box - package
+          break
+
+    suppliers_waste_lst.append(supplier_waste_int)
+  
+  if num_suppliers_fail_int == len(boxes):
+    return -1
+  else:
+    return min(suppliers_waste_lst)
+      
+          
+
+
+
+# if __name__ == '__main__':
+
+#   boxes = [[12],[11,9],[10,5,14]]
+#   packages = [3,5,8,10,11,12]
+
+#   result = linear_search(packages, boxes)
+
+#   print(result)
+      
--- problems/problem_2/p2_b.py
+++ problems/problem_2/p2_b.py
@@ -1,2 +1,32 @@
 def binary_search(packages, boxes):
-  pass
+    def binary_search_algo(boxes, package):
+      low, high = 0, len(boxes) - 1
+      while low < high:
+          mid = (low + high) // 2
+          if boxes[mid] < package:
+              low = mid + 1
+          else:
+              high = mid
+      return low
+
+    suppliers_waste_lst = []
+    num_suppliers_fail_int = 0
+
+    for supplier in boxes:
+        supplier_waste_int = 0
+        sorted_boxes = sorted(supplier)
+
+        for package in packages:
+            if package > sorted_boxes[-1]:
+                num_suppliers_fail_int += 1
+                break
+
+            index = binary_search_algo(sorted_boxes, package)
+            supplier_waste_int += sorted_boxes[index] - package
+
+        suppliers_waste_lst.append(supplier_waste_int)
+    
+    if num_suppliers_fail_int == len(boxes):
+        return -1
+    else:
+        return min(suppliers_waste_lst)  
