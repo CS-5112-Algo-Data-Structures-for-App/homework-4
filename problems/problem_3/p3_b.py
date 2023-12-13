a233f2a243c338173543d39675e80ddf6a92804b
Matthew Shen <mds377@cornell.edu>
2023-12-13 17:31:28 -0500

    finish problem 3

--- problems/problem_3/p3_b.py
+++ problems/problem_3/p3_b.py
@@ -7,46 +7,39 @@ def  local_search_2opt(cost_matrix, candidate):
         return sum
 
     
-    last_candidate = candidate
-    
-    while candidate == last_candidate: 
+    old_cost = None
+
+    while old_cost != compute_cost(cost_matrix, candidate):
+        old_cost = compute_cost(cost_matrix, candidate)
+        cost_dict = {str(candidate): old_cost}
+        # print(cost_dict)
+        n = len(candidate)
+        for i in range(n):
+            for j in range(i+1, n):
+                new_candidate = candidate.copy()
+                new_candidate[i], new_candidate[j] = new_candidate[j], new_candidate[i]
+
+                # add to cost dictionary
+                cost_dict[str(new_candidate)] = compute_cost(cost_matrix, new_candidate)
         
-        last_candidate = candidate
-        
-        for i in range(1,len(candidate)-1):
-            # Create new candidiate
-            new_candidate_front = candidate.copy()
-            new_candidate_back = candidate.copy()
-            new_candidate_front[i-1], new_candidate_front[i] = new_candidate_front[i], new_candidate_front[i-1]
-            new_candidate_back[i+1], new_candidate_back[i] = new_candidate_back[i], new_candidate_front[i+1]
-    
-            # Compute cost of both candidates
-            candidate_cost = compute_cost(cost_matrix, candidate)
-            new_candidate_front_cost = compute_cost(cost_matrix, new_candidate_front)
-            new_candidate_back_cost = compute_cost(cost_matrix, new_candidate_back)
-    
-            # set up cost dictionary
-            cost_dict = {}
-            cost_dict[candidate_cost] = candidate
-            cost_dict[new_candidate_front_cost] = new_candidate_front
-            cost_dict[new_candidate_back_cost] = new_candidate_back
-    
-            # get min cost from dict
-            min_key = min(cost_dict)
-            candidate = cost_dict[min_key]
+        # get min cost from dict
+        min_key = min(cost_dict, key=cost_dict.get)
+        str_list = min_key.strip('[]')
+        actual_list = [int(i) for i in str_list.split(',')]
+        candidate = actual_list
             
         
     return tuple(candidate)
             
             
     
-if __name__ == "__main__":
-    cost_matrix = [
-      [float('inf'), 3, 5],
-      [3, float('inf'), 7],
-      [5, 7, float('inf')]]
-    ans = local_search_2opt(cost_matrix, (2, 3, 1)),
+# if __name__ == "__main__":
+#     cost_matrix = [
+#       [float('inf'), 3, 5],
+#       [3, float('inf'), 7],
+#       [5, 7, float('inf')]]
+#     ans = local_search_2opt(cost_matrix, (2, 3, 1)),
 
-    print(ans)
+#     print(ans)
 
       
